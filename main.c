ffdc7c673692ce2348c7590711a791b6aae6b241
陈杭 <1973433180@qq.com>
2022-11-07 17:13:52 +0800

    test:阅读json跳过换行,跳过引号

---
+++ main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+static const char* skip(const char* in) { 
+	char* ptr = in + 1;
+	printf("ptr=%s\n", ptr);
+	int len = 0;
+	while (*ptr != '\"' && *ptr && ++len) {
+		printf("*ptr=%d=%c\n", *ptr, *ptr);
+		if (*ptr++ == '\\') ptr++;
+	}
+	printf("len=%d\n", len);
+		 return in; 
+}
+
+int main(int argc, const char * argv) {
+	char* p = "(\\\"Bee\\\"asd) N \n}";
+	char * p2 = skip(p);
+	//printf("%s\n", p);
+	printf("--next--%d\n", *p2);
+}
